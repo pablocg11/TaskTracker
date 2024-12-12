01c89d228b7b420c21c8b7f252bb2635973891fc
Pablo Castro <pablocastrodb@gmail.com>
2024-12-12 22:28:11 +0100

    main: init commit

---
+++ TaskTracker/TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Pablo Castro on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
